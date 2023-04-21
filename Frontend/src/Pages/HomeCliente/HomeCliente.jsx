c76f42d94129f35d4a589059ca4457c9885d24b2
alejandrolopezg <alejandroanime02@gmail.com>
2023-04-20 20:08:44 -0500

    Version 2

---
+++ Frontend/src/Pages/HomeCliente/HomeCliente.jsx
@@ -0,0 +1,19 @@
+import Navbar from '../Componets/Navbar';
+import React from 'react';
+import { useEffect} from 'react';
+
+
+export default function HomeCliente() {
+
+    useEffect(() => {
+
+
+    }, []);
+        return (
+            <div>
+                <Navbar/>
+                
+            </div>
+        );
+    
+}
